705b85aeae4131581cf00e127bd7d81034c7b576
Matthew Smith <mtscout6@gmail.com>
2013-08-16 15:17:06 -0500

    Added initial console project

---
+++ src/sabatoast-puller/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sabatoast_puller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
