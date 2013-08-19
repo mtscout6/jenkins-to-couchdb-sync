7423fd78f7bc8afe2769264ac721d41ae98ee69e
Matthew Smith <mtscout6@gmail.com>
2013-08-19 07:08:57 -0500

    Added Jenkins Build DTOs

---
+++ src/sabatoast-puller/Jenkins/DTO/Build.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace sabatoast_puller.Jenkins.DTO
+{
+    public class Build
+    {
+        public List<IBuildActions> Actions { get; set; }
+
+        public bool Building { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public int EstimatedDuration { get; set; }
+        public string Executor { get; set; }
+        public string Id { get; set; }
+        public int Number { get; set; }
+        public string Result { get; set; }
+        public string Url { get; set; }
+    }
+
+    public interface IBuildActions{}
+
+    public class BuildParameters : IBuildActions
+    {
+        public List<BuildParameter> Parameters { get; set; }
+    }
+
+    public class BuildParameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
