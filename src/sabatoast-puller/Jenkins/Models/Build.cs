7c45b520021dc9af64ecff2023d49597b1dd25f9
Matthew Smith <mtscout6@gmail.com>
2013-11-04 11:58:35 -0600

    Removed unnecessary properties

--- src/sabatoast-puller/Jenkins/Models/Build.cs
+++ src/sabatoast-puller/Jenkins/Models/Build.cs
@@ -17,14 +17,7 @@ namespace sabatoast_puller.Jenkins.Models
 
         public string Job { get; set; }
         public bool Building { get; set; }
-        public string Description { get; set; }
-        public int Duration { get; set; }
-        public int EstimatedDuration { get; set; }
-        public string Executor { get; set; }
-        public string Id { get; set; }
         public int Number { get; set; }
-        public string Result { get; set; }
-        public string Url { get; set; }
     }
 
     public interface IBuildActions{}
