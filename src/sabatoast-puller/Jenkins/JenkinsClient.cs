d50c18a99d9288611e86030657f9c588261100ca
Matthew Smith <mtscout6@gmail.com>
2013-11-05 08:48:35 -0600

    Added means to extract description meta data to json prior to pushing to couch

--- src/sabatoast-puller/Jenkins/JenkinsClient.cs
+++ src/sabatoast-puller/Jenkins/JenkinsClient.cs
@@ -8,6 +8,7 @@ using RestSharp;
 using sabatoast_puller.Couch;
 using sabatoast_puller.Jenkins.Models;
 using FubuCore;
+using Quartz.Util;
 
 namespace sabatoast_puller.Jenkins
 {
@@ -98,6 +99,7 @@ namespace sabatoast_puller.Jenkins
                                         var couchResponse = ct.Result;
 
                                         var jenkinsData = JsonConvert.DeserializeObject<JObject>(jenkinsResponse.Content);
+                                        ExtractDescriptionMetaData(jenkinsData, jenkinsResponse);
 
                                         if (modifyRawJson != null)
                                         {
@@ -128,6 +130,56 @@ namespace sabatoast_puller.Jenkins
                 }, TaskContinuationOptions.OnlyOnRanToCompletion);
 
             return responseTask.ContinueWith(t => t.Result.Data, TaskContinuationOptions.OnlyOnRanToCompletion);
+        }
+
+        void ExtractDescriptionMetaData<T>(JObject jenkinsData, IRestResponse<T> jenkinsResponse) where T : ICouchDocument
+        {
+            JToken descToken;
+            if (!jenkinsData.TryGetValue("description", out descToken) || descToken.Value<string>() == null)
+            {
+                return;
+            }
+
+            var descriptionLines = descToken.Value<string>()
+                                            .Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            var inComment = false;
+
+            foreach (var line in descriptionLines)
+            {
+                if (line.StartsWith("<!--"))
+                {
+                    inComment = true;
+                    continue;
+                }
+
+                if (line.StartsWith("-->"))
+                {
+                    inComment = false;
+                    continue;
+                }
+
+                if (!inComment || !line.StartsWith("@"))
+                {
+                    continue;
+                }
+
+                var parts = line.TrimStart('@');
+                var splitIndex = parts.IndexOf(":", StringComparison.CurrentCulture);
+                var key = parts.Substring(0, splitIndex);
+                var value = parts.Substring(splitIndex + 1);
+
+                if (value.IsNullOrWhiteSpace()) continue;
+
+                try
+                {
+                    var valueJson = JsonConvert.DeserializeObject<JObject>(value);
+                    jenkinsData["description-{0}".ToFormat(key)] = valueJson;
+                }
+                catch (Exception ex)
+                {
+                    _log.Error("Failed to parse description meta data for {0}".ToFormat(jenkinsResponse.Data._id), ex);
+                }
+            }
         }
     }
 }
